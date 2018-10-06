2cc95c0da2577f60e86c579c510ee6a142412e14
3egg <627096931@qq.com>
2018-10-06 20:09:20 +0800

    eighth day cms content page ... upload succeed and spring data redis ok

--- pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
+++ pinyougou-content-service/src/main/java/com/pinyougou/content/service/impl/ContentServiceImpl.java
@@ -1,4 +1,5 @@
 package com.pinyougou.content.service.impl;
+
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -9,144 +10,159 @@ import com.pinyougou.pojo.TbContentExample;
 import com.pinyougou.pojo.TbContentExample.Criteria;
 import entity.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 /**
  * 服务实现层
- * @author Administrator
  *
+ * @author Administrator
  */
+@SuppressWarnings("all")
 @Service
 @Transactional
 public class ContentServiceImpl implements ContentService {
 
-	@Autowired
-	private TbContentMapper contentMapper;
+    @Autowired
+    private TbContentMapper contentMapper;
 
-	@Autowired
-	//private RedisTemplate redisTemplate;
-	/**
-	 * 查询全部
-	 */
-	@Override
-	public List<TbContent> findAll() {
-		return contentMapper.selectByExample(null);
-	}
+    @Autowired
+    private RedisTemplate redisTemplate;
 
-	/**
-	 * 按分页查询
-	 */
-	@Override
-	public PageResult findPage(int pageNum, int pageSize) {
-		PageHelper.startPage(pageNum, pageSize);
-		Page<TbContent> page=   (Page<TbContent>) contentMapper.selectByExample(null);
-		return new PageResult(page.getTotal(), page.getResult());
-	}
+    /**
+     * 查询全部
+     */
+    @Override
+    public List<TbContent> findAll() {
+        return contentMapper.selectByExample(null);
+    }
 
-	/**
-	 * 增加
-	 */
-	@Override
-	public void add(TbContent content) {
+    /**
+     * 按分页查询
+     */
+    @Override
+    public PageResult findPage(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        Page<TbContent> page = (Page<TbContent>) contentMapper.selectByExample(null);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
 
-
-		contentMapper.insert(content);
-
-		//清除缓存
-		//redisTemplate.boundHashOps("content").delete(content.getCategoryId());
-	}
-
-	
-	/**
-	 * 修改
-	 * 修改前和修改后的  分类id 缓存都需要清除
-	 */
-	@Override
-	public void update(TbContent content){
-		//查询修改前的分类Id
-		Long categoryId = contentMapper.selectByPrimaryKey(content.getId()).getCategoryId();
-		//redisTemplate.boundHashOps("content").delete(categoryId);
-		contentMapper.updateByPrimaryKey(content);
-		//如果分类ID发生了修改,清除修改后的分类ID的缓存
-		/*if(categoryId.longValue()!=content.getCategoryId().longValue()){
-			redisTemplate.boundHashOps("content").delete(content.getCategoryId());
-		}*/
-	}	
-	
-	/**
-	 * 根据ID获取实体
-	 * @param id
-	 * @return
-	 */
-	@Override
-	public TbContent findOne(Long id){
-		return contentMapper.selectByPrimaryKey(id);
-	}
-
-	/**
-	 * 批量删除
-	 */
-	@Override
-	public void delete(Long[] ids) {
-		for(Long id:ids){
-			//清除缓存
-			Long categoryId = contentMapper.selectByPrimaryKey(id).getCategoryId();//广告分类ID
-			//redisTemplate.boundHashOps("content").delete(categoryId);
-			contentMapper.deleteByPrimaryKey(id);
-		}		
-	}
-	
-	
-		@Override
-	public PageResult findPage(TbContent content, int pageNum, int pageSize) {
-		PageHelper.startPage(pageNum, pageSize);
-		
-		TbContentExample example=new TbContentExample();
-		Criteria criteria = example.createCriteria();
-		
-		if(content!=null){			
-						if(content.getTitle()!=null && content.getTitle().length()>0){
-				criteria.andTitleLike("%"+content.getTitle()+"%");
-			}
-			if(content.getUrl()!=null && content.getUrl().length()>0){
-				criteria.andUrlLike("%"+content.getUrl()+"%");
-			}
-			if(content.getPic()!=null && content.getPic().length()>0){
-				criteria.andPicLike("%"+content.getPic()+"%");
-			}
-			if(content.getStatus()!=null && content.getStatus().length()>0){
-				criteria.andStatusLike("%"+content.getStatus()+"%");
-			}
-	
-		}
-		
-		Page<TbContent> page= (Page<TbContent>)contentMapper.selectByExample(example);		
-		return new PageResult(page.getTotal(), page.getResult());
-	}
-
-	@Override
-	public List<TbContent> findByCategoryId(Long categoryId) {
-
-		/*List<TbContent> contentList= (List<TbContent>)
-				redisTemplate.boundHashOps("content").get(categoryId);*/
-        List<TbContent> contentList= null;
-		if(contentList == null) {
-			//根据广告分类ID查询广告列表
-			TbContentExample contentExample=new TbContentExample();
-			Criteria criteria2 = contentExample.createCriteria();
-			criteria2.andCategoryIdEqualTo(categoryId);
-			criteria2.andStatusEqualTo("1");//开启状态
-			contentExample.setOrderByClause("sort_order");//排序
-			contentList = contentMapper.selectByExample(contentExample);
-			//redisTemplate.boundHashOps("content").put(categoryId,contentList);
+    /**
+     * 增加
+     */
+    @Override
+    public void add(TbContent content) {
 
 
-		}else {
-			System.out.println("************从缓存读取数据****************");
-		}
-		return  contentList;
-	}
-	
+        contentMapper.insert(content);
+
+        //清除缓存
+        try {
+            redisTemplate.boundHashOps("content").delete(content.getCategoryId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * 修改
+     * 修改前和修改后的  分类id 缓存都需要清除
+     */
+    @Override
+    public void update(TbContent content) {
+        //查询修改前的分类Id , 原来分类的id
+        Long id = content.getId();//通过页面传过来得id
+        TbContent tbContent = contentMapper.selectByPrimaryKey(id);//找到数据库得content对象
+        Long categoryId = tbContent.getCategoryId();//通过这个对象找到它的categoryId
+        //修改就要根据大key删除里面的field
+        redisTemplate.boundHashOps("content").delete(categoryId);
+        contentMapper.updateByPrimaryKey(content);
+        //如果分类ID发生了修改,清除修改后的分类ID的缓存
+        Long updateId = content.getCategoryId();//从页面传过来的分类id
+        if (categoryId != updateId) {
+            //如果从数据库传过来的id和页面传过来的id不一样就要把updateId这个field删除
+            redisTemplate.boundHashOps("content").delete(updateId);
+        }
+    }
+
+    /**
+     * 根据ID获取实体
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public TbContent findOne(Long id) {
+        return contentMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 批量删除
+     */
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+            //清除缓存
+            Long categoryId = contentMapper.selectByPrimaryKey(id).getCategoryId();//广告分类ID
+            redisTemplate.boundHashOps("content").delete(categoryId);
+            contentMapper.deleteByPrimaryKey(id);
+        }
+    }
+
+
+    @Override
+    public PageResult findPage(TbContent content, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+
+        TbContentExample example = new TbContentExample();
+        Criteria criteria = example.createCriteria();
+
+        if (content != null) {
+            if (content.getTitle() != null && content.getTitle().length() > 0) {
+                criteria.andTitleLike("%" + content.getTitle() + "%");
+            }
+            if (content.getUrl() != null && content.getUrl().length() > 0) {
+                criteria.andUrlLike("%" + content.getUrl() + "%");
+            }
+            if (content.getPic() != null && content.getPic().length() > 0) {
+                criteria.andPicLike("%" + content.getPic() + "%");
+            }
+            if (content.getStatus() != null && content.getStatus().length() > 0) {
+                criteria.andStatusLike("%" + content.getStatus() + "%");
+            }
+
+        }
+
+        Page<TbContent> page = (Page<TbContent>) contentMapper.selectByExample(example);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
+    @Override
+    public List<TbContent> findByCategoryId(Long categoryId) {
+
+        //这里是获取大key , 这里使用hash存取数据  key : field1 --->value1
+        //                                           field2 --->value2
+        List<TbContent> contentList = (List<TbContent>)
+                redisTemplate.boundHashOps("content").get(categoryId);
+        //这里是使用了hash数据结构来存取数据 . 大key为content . 通过不同的field(categoryId) 来获取到不同的value
+        if (contentList == null) {
+            //根据广告分类ID查询广告列表
+            TbContentExample contentExample = new TbContentExample();
+            Criteria criteria2 = contentExample.createCriteria();
+            criteria2.andCategoryIdEqualTo(categoryId);
+            criteria2.andStatusEqualTo("1");//开启状态
+            contentExample.setOrderByClause("sort_order");//排序
+            //没有值就把List<Tbcontent>这个集合数据存进数据库
+            contentList = contentMapper.selectByExample(contentExample);
+            //这里的field为categoryId spring-data-redis把它序列化了,value是list集合对象数据,也给序列化了
+            redisTemplate.boundHashOps("content").put(categoryId, contentList);
+        } else {
+            System.out.println("************从缓存读取数据****************");
+        }
+        return contentList;
+    }
+
 }
--- pinyougou-manager-web/src/main/webapp/js/service/uploadService.js
+++ pinyougou-manager-web/src/main/webapp/js/service/uploadService.js
@@ -1,20 +1,16 @@
-app.service('uploadService', function ($http) {
-    //这个上传文件的方法
-    this.uploadFile = function () {
-        //手动给他创建了一个form表单
+app.service("uploadService",function ($http) {
+    //文件上传
+    this.uploadFile  = function () {
         var formData = new FormData();
-        //然后往表单里面添加数据
-        //第一个参数：file和服务端中的controller中的参数即(MultipartFile file)一致
-        //第二个参数：file 和input 中的id一致 这里支持多图片，现在只要一个所以就选一张即可.
-        formData.append('file', file.files[0]);
-        return $http({//向controller返回一个相当于ajax
-            method: 'post',
-            url: '../upload.do',
-            data: formData,//提交的数据就是自定义的form表单里面的数据
-            //这样浏览器会帮我们把Content-Type 设置为 multipart/form-data.
-            //浏览器默认为 application/json
-            headers: {'Content-type': undefined},
-            transformRequest: angular.identity
+        formData.append("file",file.files[0]);
+        return $http({
+            method:'POST',
+            url: "../upload.do",
+            data: formData,
+            headers: {'Content-Type':undefined},
+            transformRequest:angular.identity
         });
     }
+//移除xxxxx
+
 });
